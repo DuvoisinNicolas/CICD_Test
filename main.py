d587a5a43ddf8eb5bd7b2d875416764074cc669f
Nicolas Duvoisin <duvoisin.ni@gmail.com>
2022-02-24 11:34:59 +0100

    ajout d'une erreur

--- main.py
+++ main.py
@@ -4,13 +4,23 @@ def programIsWorking():
 def programIsntWorking():
 	return -1
 
+def myBadFunction(val):
+	print("Doing many things...")
+	print("Doing even more things...")
+	val = 0
+	print("But val got unexpectedly modified !")
+	return val
+
+
 def myFunction():
 	result = -1
 	shouldBeSetTo1 = 1
+	shouldBeSetTo1 = myBadFunction(shouldBeSetTo1)
 	if shouldBeSetTo1 == 1:
 		result = programIsWorking()
 	else:
-		result = programIsntWorking()
+		result = programIsntWorking()	
+	
 	return result
 
 if __name__ == "__main__":
